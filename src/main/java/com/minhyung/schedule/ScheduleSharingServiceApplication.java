46c7df1002e68f77f4da6d8320489a17d0137130
minhyung <minhyung66547@gmail.com>
2025-07-29 16:15:18 +0900

    chore: initial project setup

---
+++ src/main/java/com/minhyung/schedule/ScheduleSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minhyung.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleSharingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minhyung/schedule/ScheduleSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhyung.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
