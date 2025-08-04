6d214edf2714faf485fd36b4768579a7368e8ddd
minhyung <minhyung66547@gmail.com>
2025-08-04 21:57:34 +0900

    feat: swagger 설정

---
+++ src/main/java/com/minhyung/schedule/common/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.minhyung.schedule.common;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("schedule-sharing-service-v1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Schedule Sharing Service API")
+                        .version("v1.0.0")
+                        .description("일정 공유 서비스 API")
+                );
+    }
+}
