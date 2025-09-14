c4fce1a3bba7a2f0af9a6b31e4664caf4c4891b1
minhyung <minhyung66547@gmail.com>
2025-09-14 22:01:58 +0900

    test: 테스트 코드 리팩터링 #14

--- src/test/java/com/minhyung/schedule/auth/LogoutIntegrationTest.java
+++ src/test/java/com/minhyung/schedule/auth/LogoutIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.minhyung.schedule.auth;
 
-import com.minhyung.schedule.AuthenticatedTest;
+import com.minhyung.schedule.AuthenticatedIntegrationTest;
 import com.minhyung.schedule.common.ApiPathsUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LogoutIntegrationTest extends AuthenticatedTest {
+public class LogoutIntegrationTest extends AuthenticatedIntegrationTest {
     private static final String REQUEST_PATH = ApiPathsUtils.auth("logout");
 
     @Autowired
