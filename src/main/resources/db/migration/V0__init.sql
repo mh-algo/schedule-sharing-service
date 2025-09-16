49519a9ff35a47f8220958ede08425135470ce53
minhyung <minhyung66547@gmail.com>
2025-09-16 22:22:09 +0900

    feat: 테이블 생성 쿼리 수정 #16
    - 개발 편의상 flyway 마이그레이션 파일 수정
    - 반드시 테이블 초기화 후 실행

--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -4,7 +4,7 @@ CREATE TABLE `users` (
     `password`	VARCHAR(100)	NOT NULL,
     `status`    TINYINT NOT NULL    DEFAULT 1   COMMENT '0: SUSPENDED, 1: ACTIVE, 2: UNVERIFIED',
     `created_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`	TIMESTAMP	NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY uk_username (`username`)
@@ -15,7 +15,7 @@ CREATE TABLE `groups` (
     `owner_id`	BIGINT	NOT NULL,
     `name`	VARCHAR(50)	NOT NULL,
     `created_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`	TIMESTAMP	NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT fk_groups_owner_id FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`)
@@ -27,7 +27,7 @@ CREATE TABLE `members` (
     `user_id`	BIGINT	NOT NULL,
     `role_type`	TINYINT	NOT NULL	DEFAULT 3	COMMENT '0: 블랙리스트, 1: 그룹장, 2: 관리자, 3: 사용자',
     `created_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`	TIMESTAMP	NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT fk_members_group_id FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`),
@@ -42,7 +42,7 @@ CREATE TABLE `schedules` (
     `date`	TIMESTAMP	NOT NULL,
     `notification_date`	TIMESTAMP	NULL,
     `created_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`	TIMESTAMP	NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT fk_schedules_group_id FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`)
@@ -74,16 +74,40 @@ CREATE TABLE `group_invites` (
 
 CREATE TABLE `notifications` (
     `id`	BIGINT	NOT NULL AUTO_INCREMENT,
+    `type`	TINYINT	NOT NULL,
+    `title`	VARCHAR(50)	NOT NULL,
+    `message`	VARCHAR(256)	NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `notification_receivers` (
+    `id`	BIGINT	NOT NULL AUTO_INCREMENT,
+    `notification_id`	BIGINT	NOT NULL,
     `receiver_id`	BIGINT	NOT NULL,
     `target_type`	TINYINT	NOT NULL,
     `target_id`	BIGINT	NOT NULL	COMMENT 'type에 해당하는 id',
-    `message`	VARCHAR(256)	NOT NULL,
+    `payload`	TEXT	NULL,
     `is_read`	TINYINT	NOT NULL	DEFAULT 0	COMMENT '0: FALSE, 1: TRUE',
     `created_at`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+    `read_at`   TIMESTAMP	NULL,
     `deleted_at`	TIMESTAMP	NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT fk_notifications_receiver_id FOREIGN KEY (`receiver_id`) REFERENCES `users`(`id`),
-    UNIQUE KEY uk_target (`target_type`, `target_id`)
+    CONSTRAINT fk_notification_receivers_notification_id FOREIGN KEY (`notification_id`) REFERENCES `notifications`(`id`),
+    CONSTRAINT fk_notification_receivers_receiver_id FOREIGN KEY (`receiver_id`) REFERENCES `users`(`id`),
+    UNIQUE KEY uk_identify_notification (`receiver_id`, `target_type`, `target_id`)
+);
+
+CREATE TABLE `notification_sending` (
+    `id`	BIGINT	NOT NULL AUTO_INCREMENT,
+    `notification_receiver_id`	BIGINT	NOT NULL,
+    `status`	TINYINT	NOT NULL    DEFAULT 0	COMMENT '0: PENDING, 1: IN_QUEUE, 2: IN_PROGRESS, 3: SENT, 4: FAILED,5: CANCELLED',
+    `attempt`	INT	NOT NULL    DEFAULT 0,
+    `next_push_at`	TIMESTAMP	NULL,
+    `version`	INT	NOT NULL    DEFAULT 0,
+    `updated_at`   TIMESTAMP	NULL    DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    CONSTRAINT fk_notification_sending_notification_receiver_id FOREIGN KEY (`notification_receiver_id`) REFERENCES `notification_receivers`(`id`),
+    UNIQUE KEY uk_notification_receiver_id (`notification_receiver_id`)
 );
 
 CREATE TABLE `group_activity_logs` (
