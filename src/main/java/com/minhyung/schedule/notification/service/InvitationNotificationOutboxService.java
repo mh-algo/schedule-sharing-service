3cd2347e38f0a6f3c2a51dc2f3c23772b43572fd
minhyung <minhyung66547@gmail.com>
2026-01-30 15:53:29 +0900

    feat: 초대 알림 저장방식 개선 #18
    - 불필요한 DB 조회를 최소화하여 쿼리 호출 횟수를 감소시키고, 처리 성능을 향상

--- src/main/java/com/minhyung/schedule/notification/service/InvitationNotificationOutboxService.java
+++ src/main/java/com/minhyung/schedule/notification/service/InvitationNotificationOutboxService.java
@@ -1,19 +1,14 @@
 package com.minhyung.schedule.notification.service;
 
 import com.minhyung.schedule.auth.domain.entity.UserEntity;
-import com.minhyung.schedule.auth.exception.UserNotFoundException;
-import com.minhyung.schedule.auth.service.UserService;
-import com.minhyung.schedule.common.exception.ApiException;
-import com.minhyung.schedule.common.exception.ServerErrorCode;
 import com.minhyung.schedule.notification.domain.NotificationData;
 import com.minhyung.schedule.notification.domain.NotificationSendingInfo;
 import com.minhyung.schedule.notification.domain.SendingStatus;
 import com.minhyung.schedule.notification.domain.entity.NotificationEntity;
-import com.minhyung.schedule.notification.domain.entity.NotificationMessageEntity;
 import com.minhyung.schedule.notification.domain.entity.NotificationOutboxEntity;
-import com.minhyung.schedule.notification.repository.NotificationMessageRepository;
 import com.minhyung.schedule.notification.repository.NotificationOutboxRepository;
 import com.minhyung.schedule.notification.repository.NotificationRepository;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,46 +18,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class InvitationNotificationOutboxService implements NotificationService {
     private final NotificationRepository notificationRepository;
     private final NotificationOutboxRepository outboxRepository;
-    private final NotificationMessageRepository messageRepository;
-    private final UserService userService;
+    private final EntityManager em;
 
     @Transactional
     @Override
     public NotificationSendingInfo createNotification(NotificationData data) {
-        // 메시지 조회
-        NotificationMessageEntity message = messageRepository.findByType(data.messageType())
-                .orElseThrow(() -> {
-                    log.error("Message Not Found: {}", data.messageType());
-                    return new ApiException(ServerErrorCode.SERVER_ERROR);
-                });
 
-        // 수신자 조회
-        UserEntity receiver;
-        try {
-            receiver = userService.getUserEntity(data.receiverId());
-        } catch (UserNotFoundException e) {
-            log.error("Receiver Not Found: {}", data.receiverId());
-            throw new ApiException(ServerErrorCode.SERVER_ERROR);
-        }
+        // 수신자
+        UserEntity receiver = em.getReference(UserEntity.class, data.receiverId());
 
         // 알림 생성
         NotificationEntity notification = NotificationEntity.builder()
-                .message(message)
                 .receiver(receiver)
                 .targetType(data.targetType())
                 .targetId(data.targetId())
                 .payload(data.payload())
                 .build();
-        NotificationEntity savedNotification = notificationRepository.save(notification);
+        notificationRepository.save(notification);
 
         // 알림 outbox 생성
         NotificationOutboxEntity outbox = NotificationOutboxEntity.builder()
-                .receiverId(receiver.getId())
+                .receiverId(data.receiverId())
                 .payload(data.payload())
                 .status(SendingStatus.READY)
                 .build();
         NotificationOutboxEntity savedOutbox = outboxRepository.save(outbox);
-        return new NotificationSendingInfo(savedOutbox.getId(), savedNotification.getReceiver().getId(),
-                savedNotification.getPayload(), savedOutbox.getAttempt());
+        return new NotificationSendingInfo(savedOutbox.getId(), data.receiverId(),
+                data.payload(), savedOutbox.getAttempt());
     }
 }
